024ec5852700bce77703ae828859c148d8c4f828
Marcel O'Neil <marcel@marceloneil.com>
2018-11-01 16:20:28 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# GitHub Auto-Pull
+
+Automatically pull various git repositories upon receiving a GitHub webhook.
+
+### Setup
+
+Copy the "\*.example" files and edit them with the required values.
+uWSGI and Python 3 are required, Pipenv is used for dependency management.
+Start the app using `uwsgi uwsgi.ini`
---
+++ main.py
@@ -0,0 +1,23 @@
+import hmac
+import subprocess
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+app.config.from_json('settings.json')
+
+@app.route('/github_webhook', methods=['POST'])
+def github_hook():
+    signature = request.headers.get('X-Hub-Signature')
+    sha, signature = signature.split('=')
+    secret = str.encode(app.config.get('GITHUB_SECRET'))
+    hashhex = hmac.new(secret, request.data, digestmod='sha1').hexdigest()
+    if hmac.compare_digest(hashhex, signature):
+        proc = subprocess.Popen("./pull.sh")
+        proc.wait()
+        return jsonify({}), 200
+    else:
+        return jsonify({}), 403
+
+if __name__ == '__main__':
+    app.run()
